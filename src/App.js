3324ffc39794d93dfeb6facac0e34b6da5517f7c
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-08-03 16:09:56 -0300

    Tentativa Fix

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FilterProvider } from './hooks/useFilterContext';
 import logo from './assets/generatedtext (1).png';
-
+// eslint-disable-next-line max-len
+import backgroundImage from './assets/star-wars-space-background-1920-x-1080-f1o3qr8zm0z4662c (1).jpg';
 import PlanetsTable from './components/PlanetsTable';
 
 function App() {
   return (
     <FilterProvider>
+      <img
+        src={ backgroundImage }
+        alt="Background"
+        className="bg-image sm:h-full sm:object-cover"
+      />
       <header
         className="header-logo"
       >
